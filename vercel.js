704b6faf874b7d109acf3845202ab15212522a1c
madupoorna <madupoorna9@gmail.com>
2025-08-19 18:46:34 +0530

    updated vercel.js

--- vercel.js
+++ vercel.js
@@ -1,14 +1,8 @@
 {
   "version": 2,
   "builds": [
-    {
-      "src": "api/backend.py",
-      "use": "@vercel/python"
-    },
-    {
-      "src": "public/**/*",
-      "use": "@vercel/static"
-    }
+    { "src": "api/backend.py", "use": "@vercel/python" },
+    { "src": "public/**/*", "use": "@vercel/static" }
   ],
   "routes": [
     { "src": "/api/(.*)", "dest": "api/backend.py" },
